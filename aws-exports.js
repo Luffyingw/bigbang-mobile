e1813a139c9431a93d029835bd5f43b005d04d3f
JasonZhu <jasonzhu810@gmail.com>
2018-04-29 01:24:38 -0400

    add vscode configuration, storyboardUI

--- aws-exports.js
+++ aws-exports.js
@@ -1,7 +1,7 @@
 
 export default {
-  "graphqlEndpoint": "https://eb4b3g55prfdplsu2oxl27insy.appsync-api.us-east-1.amazonaws.com/graphql",
+  "graphqlEndpoint": "",
   "region": "us-east-1",
-  "apiKey": "da2-b2rcnu6c2zhq7gk2uijks3265e",
+  "apiKey": "",
   "authType": "API_KEY"
 }
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { AppRegistry } from 'react-native';
 import App from './App';
+import StorybookUI from './storybook';
 
 console.ignoredYellowBox = ['Remote', 'Warning']
 
 AppRegistry.registerComponent('RNCities', () => App);
+
